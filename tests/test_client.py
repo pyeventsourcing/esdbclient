bc2f22242fc571534c5df125c1d1f246e9caff38
johnbywater <john.bywater@appropriatesoftware.net>
2025-10-13 20:14:42 +0100

    Enabled test_extra_checkpoint_bug_is_fixed.

--- tests/test_client.py
+++ tests/test_client.py
@@ -2681,7 +2681,7 @@ class TestKurrentDBClient(KurrentDBClientTestCase):
         )
 
     @skipIf("21.10" in KURRENTDB_DOCKER_IMAGE, "'Extra checkpoint' bug not fixed")
-    def _test_extra_checkpoint_bug_is_fixed(self) -> None:
+    def test_extra_checkpoint_bug_is_fixed(self) -> None:
         self.construct_esdb_client()
 
         # Append new events.
